637b24e0805b272de0934ced95f226c107fb179b
tharwatsamy <tharwatsamy2004@gmail.com>
2022-08-25 05:28:10 +0200

    add simple bloc observer

---
+++ lib/core/utils/simple_bloc_observer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver implements BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    print(change);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {}
+
+  @override
+  void onCreate(BlocBase bloc) {}
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {}
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+  }
+}
