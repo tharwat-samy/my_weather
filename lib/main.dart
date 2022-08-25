ff2b2b934c903df7bf8c7bd6ef854f546bbe661a
tharwatsamy <tharwatsamy2004@gmail.com>
2022-08-25 04:13:48 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myweather/Features/splash/presentaion/views/splash_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyWeather());
 }
 
